2cfe70211d7fbf0d5bed2e671d6fc0b7590a1078
Vika Lubenets <lubenetcvika@gmail.com>
2023-04-08 18:44:06 +0400

    feat: add burger menu functionality

---
+++ shelter/pages/main/script.js
@@ -0,0 +1,18 @@
+/* burger menu start */
+
+let menuBtn = document.querySelector('.burger-menu');
+
+let navigation = document.querySelector('.nav');
+
+let header = document.querySelector('.header__content');
+
+menuBtn.addEventListener('click', function(){
+	header.classList.toggle('open');
+})
+
+
+/* burger menu end */
+
+/* slider start */
+
+/* slider start */
---
+++ shelter/pages/pets/script.js
@@ -0,0 +1,16 @@
+/* burger menu start */
+
+let menuBtn = document.querySelector('.burger-menu');
+
+let header = document.querySelector('.header');
+
+menuBtn.addEventListener('click', function(){
+	header.classList.toggle('open');
+})
+
+
+/* burger menu end */
+
+/* slider start */
+
+/* slider start */
