f99939a01ae4a5dfa2bece2977780d0c2246854d
Vika Lubenets <lubenetcvika@gmail.com>
2023-05-15 16:55:31 +0400

    feat: create game field using JS

--- minesweeper/src/index.js
+++ minesweeper/src/index.js
@@ -12,5 +12,39 @@ const GAME_FIELD = document.createElement('div');
 GAME_FIELD.classList.add('game-field');
 GAME_CONTAINER.appendChild(GAME_FIELD);
 
-//
+// create cells in the game field
+
+let cells_number = 10;
+let mins_number = 6;
+
+function createField (cells, mins) {
+
+    let field = [];
+
+    for(let i = 0; i < cells; i++){
+        let row = [];
+        for(let j = 0; j < cells; j++){
+            let cell = document.createElement('div');
+            cell.classList.add('game-field__item');
+            let item = {
+                cell,
+                i,
+                j,
+            }
+            row.push(item);
+        }
+        field.push(row);
+    }
+
+    return field;
+
+}
+
+let field = createField(cells_number, mins_number);
+field.forEach(row =>{
+    row.forEach(item => {
+        GAME_FIELD.appendChild(item.cell);
+    })
+})
+
 
