d1eae61837fd8c7281d2b7f4eb149b4ffd7c6e62
Vika Lubenets <lubenetcvika@gmail.com>
2023-05-16 13:59:47 +0400

    feat: add function for mines generation and prevent default for right click

--- minesweeper/src/index.js
+++ minesweeper/src/index.js
@@ -1,4 +1,4 @@
-// create basic html 
+// create basic html containers
 const BODY = document.querySelector('body');
 const GAME_CONTAINER = document.createElement('div');
 GAME_CONTAINER.classList.add('game-container');
@@ -12,14 +12,14 @@ const GAME_FIELD = document.createElement('div');
 GAME_FIELD.classList.add('game-field');
 GAME_CONTAINER.appendChild(GAME_FIELD);
 
-// create cells in the game field
+// create cells in the game-field
 
 let cells_number = 10;
-let mins_number = 6;
+let mines_number = 10;
 
 GAME_FIELD.style.setProperty('--size', cells_number)
 
-function createField (cells, mins) {
+function createField (cells) {
 
     let field = [];
 
@@ -32,6 +32,7 @@ function createField (cells, mins) {
                 cell,
                 i,
                 j,
+                mine: false,
             }
             row.push(item);
         }
@@ -42,11 +43,65 @@ function createField (cells, mins) {
 
 }
 
-let field = createField(cells_number, mins_number);
+let field = createField(cells_number);
 field.forEach(row =>{
     row.forEach(item => {
         GAME_FIELD.appendChild(item.cell);
     })
 })
+
+// generate mines positions 
+
+const getRandomPosition = (num) => Math.floor(Math.random() * num);
+
+function createMinesPosition(cells, mines){
+    let positions = [];
+    while(positions.length <= mines){
+        let position = {
+            row: getRandomPosition(cells),
+            line: getRandomPosition(cells),
+        }
+        if(!positions.includes(position)){
+            positions.push(position);
+        }
+    }
+
+    return positions;
+}
+
+// add mines after first click in the field
+
+let game_cell = document.querySelectorAll('.game-field__item');
+let cell_opened = document.querySelectorAll('.item_opened');
+let cell_mine = document.querySelectorAll('..item_mine');
+let cell_number = document.querySelectorAll('.item_number');
+let cell_flagged = document.querySelectorAll('.item_flagged');
+
+const generateMinesOnField = () => {
+    let positions = createMinesPosition(cells_number, mines_number);
+    field.forEach(row =>{
+        row.forEach(item => {
+            positions.forEach(position =>{
+                if (item.i === position.row && item.j === position.line) {
+                    item.mine = true;
+                  }
+            })
+        })
+    })
+}
+
+field.forEach(row =>{
+    row.forEach(item => {
+       item.cell.addEventListener('contextmenu', e => {
+        e.preventDefault();
+       })
+       item.cell.addEventListener('click', e => {
+        
+       })
+    })
+})
+
+
+
 
 
