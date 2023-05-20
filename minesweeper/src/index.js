c441e24f9f94811811bb602bf1c75e606a077914
Vika Lubenets <lubenetcvika@gmail.com>
2023-05-20 22:12:46 +0400

    feat: add function for opening all blank cells

--- minesweeper/src/index.js
+++ minesweeper/src/index.js
@@ -62,12 +62,12 @@ const getRandomPosition = (num) => Math.floor(Math.random() * num);
 
 function createMinesPosition(cells, mines){
     let positions = [];
-    while(positions.length <= mines){
+    while (positions.length <= mines - 1) {
         let position = {
             row: getRandomPosition(cells),
-            line: getRandomPosition(cells),
+            col: getRandomPosition(cells),
         }
-        if(!positions.includes(position)){
+        if (!positions.some(pos => pos.row === position.row && pos.col === position.col)) {
             positions.push(position);
         }
     }
@@ -75,19 +75,30 @@ function createMinesPosition(cells, mines){
     return positions;
 }
 
-// add mines after first click in the field
+// add mines after first click in the field and open cell
 
-let game_cell = document.querySelectorAll('.game-field__item');
-let cell_opened = document.querySelectorAll('.item_opened');
-let cell_mine = document.querySelectorAll('.item_mine');
-let cell_flagged = document.querySelectorAll('.item_flagged');
+let firstClick = false;
+
+let flagged_right = 0;
+let flags_used = 0;
+let remaining_flags = 0;
+let game_time = 0;
+let game_clicks = 0;
+let game_start_time;
+
+let updateGameTime = () => {
+    const currentTime = new Date().getTime();
+    game_time = Math.floor((currentTime - game_start_time) / 1000);
+    TIMER.textContent = game_time + "\n seconds";
+};
 
 const generateMinesOnField = () => {
-    let positions = createMinesPosition(cells_number - 1, mines_number);
+    game_start_time = new Date().getTime();
+    let positions = createMinesPosition(cells_number, mines_number);
     field.forEach(row =>{
         row.forEach(item => {
             positions.forEach(position =>{
-                if (item.i === position.row && item.j === position.line) {
+                if (item.i === position.row && item.j === position.col) {
                     item.mine = true;
                   }
             })
@@ -98,16 +109,33 @@ const generateMinesOnField = () => {
 const flagOnCell = (item) => {
     if (!item.cell.classList.contains('item_flagged')) {
         item.cell.classList.add('item_flagged');
+        flags_used++;
+        remaining_flags = mines_number - flags_used;
+        FLAGS.textContent = remaining_flags + "\n flags remain";
+        if (item.mine) {
+            flagged_right++;
+            console.log(flagged_right);
+        }
       } else {
         item.cell.classList.remove('item_flagged');
+        flags_used--;
+        remaining_flags = mines_number - flags_used;
+        FLAGS.textContent = remaining_flags + "\n flags remain";
+        if(item.mine){
+            flagged_right--;
+        }
       }
+
+    if (flagged_right === mines_number) {
+        gameWin();
+    }
 };
 
-const getNumberOfMines = (field, row, line) => {
+const getNumberOfMines = (field, row, col) => {
     let count = 0;
 
     for(let i = row - 1; i <= row + 1; i++){
-        for(let j = line - 1; j <= line + 1; j++){
+        for(let j = col - 1; j <= col + 1; j++){
             if(i >= 0 && j >= 0 && i < field.length && j < field.length){
                 if(field[i][j].mine){
                     count++;
@@ -131,25 +159,60 @@ const showMines = (field) => {
 
 const gameOver = () => {
     let text = document.createElement('div');
-    text.textContent = 'Game is over! Try again';
+    text.textContent = 'Game over. Try again';
     text.classList.add('text');
     BODY.appendChild(text);
     showMines(field);
+    updateGameTime();
 };
+
+let gameWin = () => {
+    let textWin = document.createElement('div');
+    textWin.textContent = `Hooray! You found all mines in ${game_time} seconds and ${game_clicks} moves!`;
+    textWin.classList.add('text-win');
+    BODY.appendChild(textWin);
+    updateGameTime();
+};
+
+const openBlanckCells = (item) => {
+
+    let {i , j} = item;
+
+    for (let row = i - 1; row <= i + 1; row++) {
+        for (let col = j - 1; col <= j + 1; col++) {
+          if (row >= 0 && col >= 0 && row < field.length && col < field.length) {
+            const currItem = field[row][col];
+            if (!currItem.cell.classList.contains('item_opened') && !currItem.mine) {
+              openCell(currItem);
+            }
+          }
+        }
+      }
+
+}
 
 const openCell = (item) => {
 
-    if(item.mine){
-        gameOver();
-    } else {
+    if (!firstClick) {
+        generateMinesOnField();
+        firstClick = true;
+    }
 
+    if (item.mine) {
+        gameOver();
+      } else {
         let numberOfMines = getNumberOfMines(field, item.i, item.j);
         item.cell.classList.add('item_opened');
-        if(numberOfMines > 0){
-            item.cell.textContent = numberOfMines;
+        if (numberOfMines === 0) {
+           openBlanckCells(item);
+        } else {
+          item.cell.textContent = numberOfMines;
         }
+      }
 
-    }
+    updateGameTime();
+    TIMER.textContent = game_time + "\n seconds";
+
 };
 
 // click on cell
@@ -159,14 +222,38 @@ field.forEach(row =>{
        item.cell.addEventListener('contextmenu', e => {
         e.preventDefault();
         flagOnCell(item);
+        updateGameTime();
+        TIMER.textContent = game_time + "\n seconds";
        })
        item.cell.addEventListener('click', e => {
-        e.openCell(item);
-        generateMinesOnField();
+        game_clicks++;
+        GAME_CLICKS.textContent = game_clicks + "\n clicks";
+        openCell(item);
+        updateGameTime();
+        TIMER.textContent = game_time + "\n seconds";
+
        })
     })
 })
 
+// create game clicks and timer
+
+const GAME_CLICKS = document.createElement('div');
+GAME_CLICKS.classList.add('game-clicks');
+GAME_MENU.appendChild(GAME_CLICKS);
+GAME_CLICKS.textContent = game_clicks + "\n clicks";
+
+const TIMER = document.createElement('div');
+TIMER.classList.add('game-timer');
+GAME_MENU.appendChild(TIMER);
+TIMER.textContent = game_time + "\n seconds";
+
+// remaining flags
+
+const FLAGS = document.createElement('div');
+FLAGS.classList.add('flags-container');
+GAME_MENU.appendChild(FLAGS);
+FLAGS.textContent = remaining_flags + "\n flags remain";
 
 
 
