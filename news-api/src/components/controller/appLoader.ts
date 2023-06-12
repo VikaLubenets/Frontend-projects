74398389ef8e0ab5b5d2c237ee6960025d4c46b5
Vika Lubenets <lubenetcvika@gmail.com>
2023-06-12 15:13:38 +0400

    fix: update URL for loading app from remoute repository

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '98627e49b8bf465fa64623b6b7c4733d', // получите свой ключ https://newsapi.org/
         });
     }
